4c5210433c673b27698b11966149f54b6cdaf91d
rajrsingh <rsingh@opengeospatial.org>
2013-05-01 11:26:01 -0400

    added viewport setting

--- b2b/index.php
+++ b2b/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<meta http-equiv="Pragma" content="no-cache">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta name="apple-mobile-web-app-capable" content="yes">
 	<title>OpenPOIs Repository Business Listing Submission</title>
 	<link type="text/css" rel="stylesheet" href="../css/MyFontsWebfontsKit.css">
 	<link rel="stylesheet" href="../css/poi.css" type="text/css">
